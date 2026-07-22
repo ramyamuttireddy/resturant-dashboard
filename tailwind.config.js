8ba4711c70e64c5fde697330e2351b29a2140dbf
Ramya Muttireddy <ramyamuttireddy@gmail.com>
2026-07-22 15:28:52 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+  content:[
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
 
-  theme: {
-    extend: {},
+  safelist:[
+    "from-[#728D3E]",
+    "to-[#5F7632]",
+    "from-[#CD7D1C]",
+    "to-[#A85F0A]",
+    "text-[#6F8F3D]",
+    "bg-[#F7F6EF]"
+  ],
+
+  theme:{
+    extend:{},
   },
 
-  plugins: [],
+  plugins:[],
 }
